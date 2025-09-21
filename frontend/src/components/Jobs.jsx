442a28426d67d41bc15d427999aa2aa7fb7ef4ec
vidya <vidyasdalabanjan@gmail.com>
2025-09-21 17:56:36 +0530

    home changed

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -27,15 +27,15 @@ const Jobs = () => {
     <div>
       <Navbar />
       <div className="max-w-7xl mx-auto mt-5">
-        <div className="flex gap-5">
-          <div className="w-1/5">
+        <div className="flex flex-col lg:flex-row gap-5">
+          <div className="w-full lg:w-1/5">
             <FilterCard />
           </div>
           {filterJobs.length <= 0 ? (
             <span>Job Not Found</span>
           ) : (
             <div className="flex-1 h-[88vh] overflow-auto pb-5">
-              <div className="grid grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {filterJobs.map((job) => (
                   <motion.div
                     initial={{ opacity: 0, x: 100 }}
