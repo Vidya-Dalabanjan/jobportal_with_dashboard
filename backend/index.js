4003c7529ec08003103dc6d37d77658a7c58c1ed
vidya <vidyasdalabanjan@gmail.com>
2025-09-21 13:32:31 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
 dotenv.config();
 const app = express();
+
+const _dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -32,6 +36,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*wildcard", (_, res) => {
+  res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+});
 app.listen(PORT, () => {
   connectDB();
   console.log(`server running at port ${PORT}`);
