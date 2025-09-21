54e26fba0765b7f17cb0dfb710629fe9834730d3
vidya <vidyasdalabanjan@gmail.com>
2025-09-21 18:01:00 +0530

    home changed

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -35,17 +35,17 @@ const FilterCard = () => {
   }, [selectedValue, dispatch]);
 
   return (
-    <div className="w-full bg-white p-3 rounded-medium">
+    <div className="w-full bg-white p-3 rounded-md shadow-sm">
       <h1 className="font-bold text-lg">Filter Jobs</h1>
       <hr className="mt-3" />
       <RadioGroup value={selectedValue} onValueChange={changeHandler}>
         {filterData.map((data, index) => (
-          <div>
+          <div className="my-4">
             <h1 className="font-bold text-lg">{data.filterType}</h1>
             {data.array.map((item, idx) => {
               const itemId = `id${index}-${idx}`;
               return (
-                <div className="flex items-center space-x-2 my-2">
+                <div className="flex flex-wrap items-center gap-2 my-2">
                   <RadioGroupItem value={item} id={itemId} />
                   <Label htmlFor={itemId}>{item}</Label>
                 </div>
